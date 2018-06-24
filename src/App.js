46d6fa0c4cf796ed3f8fcc3364e086a83bf98d44
kylexiao73 <xiao.lin@husky.neu.edu>
2018-06-24 19:08:46 -0400

    add Register.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Header} from './Header';
+import { Header } from './Header';
+import { Register } from "./Register";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header/>
+          <Header/>
+          <Register/>
       </div>
     );
   }
